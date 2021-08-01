b7d7030fc08425b68a0c4b42891730c2997ed65e
Arpit Rulania <arpitkingr@example.com>
2021-08-02 02:39:09 +1000

    constructors

--- include/gdwg/graph.hpp
+++ include/gdwg/graph.hpp
@@ -1,10 +1,10 @@
 #ifndef GDWG_GRAPH_HPP
-#	define GDWG_GRAPH_HPP
-#	include <iterator>
-#	include <map>
-#	include <memory>
-#	include <set>
-#	include <utility>
+#define GDWG_GRAPH_HPP
+#include <iterator>
+#include <map>
+#include <memory>
+#include <set>
+#include <utility>
 
 // TODO: Make this graph generic
 //       ... this won't just compile
@@ -27,14 +27,11 @@ namespace gdwg {
 				return *a < *b;
 			}
 			// this overload is used when the map.find(something) is called.
-			// here something is templated as the user might do anything.
 			// comparator made for both lhs and rhs cases.
-			template<typename t>
-			auto operator()(std::shared_ptr<N> const& a, t const& b) const noexcept -> bool {
+			auto operator()(std::shared_ptr<N> const& a, N const& b) const noexcept -> bool {
 				return *a < b;
 			}
-			template<typename t>
-			auto operator()(t const& a, std::shared_ptr<N> const& b) const noexcept -> bool {
+			auto operator()(N const& a, std::shared_ptr<N> const& b) const noexcept -> bool {
 				return a < *b;
 			}
 		};
@@ -44,8 +41,8 @@ namespace gdwg {
 			// this overload will sort the destination node(edges) in increasing order.
 			// if the destination node is the same then it is sorted by weight "E"
 			bool operator()(edgePair const& a, edgePair const& b) const {
-				return ((*(a.first) < *(b.first))
-				        || ((*(a.first) == *(b.first)) && (a.second < b.second)));
+				return ((*((a.first).lock()) < *((b.first).lock()))
+				        || ((*((a.first).lock()) == *((b.first).lock())) && (a.second < b.second)));
 			}
 		};
 
@@ -57,9 +54,20 @@ namespace gdwg {
 		**          Custom Iterator           **
 		**                                    **
 		***************************************/
+		struct value_type {
+			N from;
+			N to;
+			E weight;
+		};
 
 		class iterator {
 		public:
+			/*using value_type = graph<N, E>::value_type;
+			using reference = value_type;
+			using pointer = void;
+			using difference_type = std::ptrdiff_t;
+			using iterator_category = std::bidirectional_iterator_tag;*/
+
 		private:
 			typename std::map<std::shared_ptr<N>, destination_node, mapComparator>::const_iterator outer_iterator;
 			typename destination_node::const_iterator inner_iterator;
@@ -73,15 +81,16 @@ namespace gdwg {
 		***************************************/
 
 		// Default constructor to create an empty map.
-		graph() = default;
+		graph() noexcept = default;
 
 		// Create a graph using nodes from an initialiser list.
-		graph(std::initializer_list<N> il) {
+		graph(std::initializer_list<N> il) noexcept {
 			for (auto i = il.begin(); i != il.end(); ++i) {
 				insert_node(*i);
 			}
 		}
 
+		// Creating a graph given all the start and end iter of an abject.
 		template<typename InputIt>
 		graph(InputIt first, InputIt last) {
 			for (auto i = first; i != last; ++i) {
@@ -89,23 +98,43 @@ namespace gdwg {
 			}
 		}
 
-		graph(graph&& other) noexcept = default; // prefered method.
-		/*graph(graph&& other) noexcept {
-		   graph_ = std::exchange(other.graph_, 0u);   //not sure if this would work
+		// Move constructor to create the graph.
+		graph(graph&& other) noexcept = default;
 
-		   for(auto i = other.graph_.begin(); i != other.graph_.end(); ++i) {
-		      insert_node(*i->first); //should it be * or **
-		   }
-		   for(auto i = other.graph_.begin(); i != other.graph_.end(); ++i) {
-		      for(auto j = i->second.begin(); j != i->second.end(); ++j) {
-		         auto node = graph_.find(*(j.first));
-		         std::weak_ptr<N> weak1 = node;  //is this even correct?
-		         std::pair <std::weak_ptr<N>, E> edge1 (weak1,j.second);
-		         graph_[node].insert(edge1);
-		      }
-		   }
-		   other.graph_.clear();
-		}*/
+		// Move operator to move-assign all the nodes of an old graph.
+		auto operator=(graph&& other) noexcept -> graph& = default;
+		// Graph<N, E>& operator=(Graph<N, E>&& old) noexcept = default;
+
+		// Copy constructor to copy the whole old graph.
+		graph(graph const& other) {
+			// First we copy the shared pointer to the source(outer) node.
+			// Note: We copy pointer to already existing memory because we dont modify the memory
+			// anywhere.
+			for (auto i = other.graph_.begin(); i != other.graph_.end(); ++i) {
+				auto sptr = i->first;
+				auto exist = graph_.find(*sptr);
+				if (exist == graph_.end()) {
+					graph_[sptr] = destination_node();
+				}
+			}
+			// Next we copy all the destination(edges) nodes.
+			// Outer loop to iterate over the map.
+			for (auto i = other.graph_.begin(); i != other.graph_.end(); ++i) {
+				// Inner loop to iterate over the set.
+				if(i->second.size() != 0) {
+					for (auto j = i->second.begin(); j != i->second.end(); ++j) {
+						//auto node = graph_.find(*(j.first));
+						auto p1 = std::get<0>(*j).lock();
+						auto node = graph_.find(p1);
+						// Create a weak copy of the shared ptr.
+						std::weak_ptr<N> weak1 = node->first;
+						// Create a pair of weak ptr,E to insert into the set.
+						std::pair<std::weak_ptr<N>, E> edge1(weak1, std::get<1>(*j));
+						graph_[node->first].insert(edge1);
+					}
+				}
+			}
+		}
 
 		/***************************************
 		**                                    **
@@ -123,12 +152,6 @@ namespace gdwg {
 			return false;
 		}
 
-		struct value_type {
-			N from;
-			N to;
-			E weight;
-		};
-
 		/***************************************
 		**                                    **
 		**            Accessors               **
@@ -139,15 +162,14 @@ namespace gdwg {
 			return (graph_.find(value) != graph_.end());
 		}
 
+		[[nodiscard]] auto empty() -> bool { return graph_.empty(); }
+
+		// Function that returns total number of nodes for testing purposes.
+		[[nodiscard]] auto size() -> int { return static_cast<int>(graph_.size()); }
+
 	private:
 		std::map<std::shared_ptr<N>, destination_node, mapComparator> graph_;
-		// std::map<std::shared_ptr<N>, std::set<std::pair<std::shared_ptr<N>, E>>, mapComparator>
-		// graph_; std::map<std::shared_ptr<N>, std::set<std::shared_ptr<std::pair<std::shared_ptr<N>,
-		// E>>>, mapComparator> graph_;
 	};
 } // namespace gdwg
 
 #endif // GDWG_GRAPH_HPP
-
-// do we need to use custom iterator for modifiers etc
-// should we use custom iterators?
--- test/graph/graph_test1.cpp
+++ test/graph/graph_test1.cpp
@@ -3,10 +3,63 @@
 #include <catch2/catch.hpp>
 #include <iostream>
 
+// This is the CONSTRUCTOR TESTING file.
+
 TEST_CASE("basic test") {
 	auto g = gdwg::graph<int, std::string>{};
 	auto n = 5;
 	g.insert_node(n);
+	CHECK(!g.empty());
 	CHECK(g.is_node(n));
-	//std::cout << g.find(n);
+}
+
+TEST_CASE("test to see if empty constructor works") {
+	auto g = gdwg::graph<int, std::string>();
+	CHECK(g.empty());
+}
+
+TEST_CASE("test to see if initialiser list constructor works") {
+	auto g = gdwg::graph<int, std::string>{1,5,7,1,4,8};
+	CHECK(!g.empty());
+	// here size is not 6 because one 1 is duplicate.
+	CHECK(g.size() == 5);
+	CHECK(g.is_node(1));
+	CHECK(g.is_node(5));
+	CHECK(!g.is_node(12));
+}
+
+TEST_CASE("test to see if move constructor works") {
+	auto g = gdwg::graph<int, std::string>{1,5,7,1,4,8};
+	auto h = gdwg::graph<int, std::string>(std::move(g));
+	// because of move g should be empty and h should not.
+	CHECK(g.empty());
+	CHECK(!h.empty());
+	// here size is not 6 because one 1 is duplicate.
+	CHECK(h.size() == 5);
+	CHECK(h.is_node(5));
+	CHECK(!h.is_node(12));
+}
+
+TEST_CASE("test to see if move operator works") {
+	auto g = gdwg::graph<int, std::string>{1,5,7,1,4,8};
+	auto h = std::move(g);
+	// because of move g should be empty and h should not.
+	CHECK(g.empty());
+	CHECK(!h.empty());
+	// here size is not 6 because one 1 is duplicate.
+	CHECK(h.size() == 5);
+	CHECK(h.is_node(5));
+	CHECK(!h.is_node(12));
+}
+
+TEST_CASE("test to see if copy constructor works") {
+	auto g = gdwg::graph<int, std::string>{1,5,7,1,4,8};
+	auto h = gdwg::graph<int, std::string>(g);
+	// here size is not 6 because one 1 is duplicate.
+	CHECK(g.size() == 5);
+	CHECK(h.size() == 5);
+	CHECK(h.is_node(5));
+	CHECK(!h.is_node(12));
+	CHECK(g.is_node(7));
+	CHECK(!g.is_node(55));
 }
